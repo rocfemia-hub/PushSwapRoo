615709f65d782e54a4618a1e62fddf0b4efb57a4
Your Name <you@example.com>
2025-03-09 20:11:59 +0100

    ya no se q hago

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,11 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 20:32:42 by roo               #+#    #+#             */
-/*   Updated: 2025/03/09 19:36:35 by roo              ###   ########.fr       */
+/*   Updated: 2025/03/09 20:11:16 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-//freepsw?
 
 int main(int argc, char **argv)
 {
@@ -53,14 +51,10 @@ int	ft_next_step(int argc, char **argv, char **argvn, t_stack **stack_a)
 	}
 	else
 	{
-		//while (argv[++i]) //funcion para comprobar los numeros dentro de los argumentos entre comillas 
-		while (argv[++i]) //funcion para comprobar los numeros dentro de los argumentos entre comillas 
+		while (argv[++i]) 
 		{
 			if(ft_if_numb(argv[i], argvn, stack_a) == -1)
 				return(-1);
-			/* if (ft_nums_overflow(argv[i]) == -1)
-				return(ft_printf("Error_3\n"), ft_free_stack(stack_a), -1);
-			ft_stackadd_back(stack_a, ft_stacknew(ft_atoi2(argv[i]))); */
 		}
 	}
 	return(0);
@@ -68,16 +62,12 @@ int	ft_next_step(int argc, char **argv, char **argvn, t_stack **stack_a)
 
 int ft_next_next_step(t_stack *stack_a, t_stack *stack_b, t_vars *var)
 {	
-	// gestionar numeros del 1 al 5 y sin argumentos
-	/* if (ft_stacksize(stack_a) < 6)
-		return (ft_printf("Error??\n"), free(var), -1); */
 	var->num_ops = 0;
     var->len_a = ft_stacksize(stack_a);
     var->len_b = ft_stacksize(stack_b);
 	if (var->len_a == 0)
 		return (ft_printf("Error_4\n"), free(var), 0);
     ft_min_max(var, stack_b);
-	//ft_rare_parsing(&stack_a, &stack_b, var);
 	if(ft_if_repeat(stack_a) != 1)
 			return(ft_printf("Error_5\n"), ft_free_stack(&stack_a), 0);
 	if(ft_if_ascending(stack_a) == 1)
